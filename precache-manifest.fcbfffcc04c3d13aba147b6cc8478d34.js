d8b3e405856c6b050c9270e1ee3bd7128b0154a2
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-14 15:06:16 +0530

    Updates

---
+++ precache-manifest.fcbfffcc04c3d13aba147b6cc8478d34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9573d7c64d1af677ab158b1291b433",
+    "url": "/Color-Generator-React-Project/index.html"
+  },
+  {
+    "revision": "5e8061b0fa899e824eb9",
+    "url": "/Color-Generator-React-Project/static/css/main.3d53e6bc.chunk.css"
+  },
+  {
+    "revision": "9a9e8d0e68fad30de2f1",
+    "url": "/Color-Generator-React-Project/static/js/2.c6fdfa34.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/Color-Generator-React-Project/static/js/2.c6fdfa34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e8061b0fa899e824eb9",
+    "url": "/Color-Generator-React-Project/static/js/main.5a5b26ca.chunk.js"
+  },
+  {
+    "revision": "c88a920e4e09aa542090",
+    "url": "/Color-Generator-React-Project/static/js/runtime-main.2cd749ce.js"
+  }
+]);
