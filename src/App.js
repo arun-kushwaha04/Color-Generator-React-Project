648ca6ff9c034bd7fcd442b801742f7e729bf86c
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-14 11:32:05 +0530

    Basic structure created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import SingleColor from './SingleColor'
 import Values from 'values.js'
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color,setColor] = useState('');
+  const [error,setError] = useState(false);
+
+  const handleSubmit = ()=>{
+    console.log('hi');
+  }
+  return (
+    <>
+      <section className="container">
+        <h3>Color Genrator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" 
+            value={color}
+            onChange={(e)=>setColor(e.target.value)}
+            placeholder="#f15025"
+          />
+          <button className="btn">generate</button>
+        </form>
+      </section>
+      <section className="colors">
+        color list to be displayed here
+      </section>
+    </>
+  )
 }
 
 export default App
