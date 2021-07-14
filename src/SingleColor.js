da4770cb495be801b96cb67138638332510683f6
arun-kushwaha04 <arunkushwaha89765@gmail.com>
2021-07-14 11:59:40 +0530

    Created Color display block

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
 
-const SingleColor = () => {
-  return <h4>single color</h4>
+const SingleColor = ({rgb,weight,index}) => {
+  let colorValue = rgb.join(',');
+  const hex = rgbToHex(...rgb);
+  const [alert,setAlert] = useState(false);
+  
+  return <article className={`color ${index > 10 && 'color-light'}`} style={{backgroundColor: `rgb(${colorValue})`}}>
+    <p className="percent-value">{weight}%</p>
+    <p className="color-value">{hex}</p>
+  </article>
 }
 
 export default SingleColor
